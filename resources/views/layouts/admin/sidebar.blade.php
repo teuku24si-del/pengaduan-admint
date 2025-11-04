9faadb4775e656e4fb5968fb25bb801a069975f1
geta2125 <geta24si@mahasiswa.pcr.ac.id>
2025-11-04 07:35:41 +0700

    perubahan sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -29,7 +29,7 @@
 
                         </a>
                     </li>
-                    
+
                     <li class="nav-item">
                         <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false"
                             aria-controls="auth">
@@ -48,51 +48,15 @@
                             </ul>
                         </div>
                     </li>
-                    <li class="nav-item documentation-link">
-                        <a class="nav-link"
-                            href="http://www.bootstrapdash.com/demo/connect-plus-free/jquery/documentation/documentation.html"
-                            target="_blank">
-                            <span class="icon-bg">
-                                <i class="mdi mdi-file-document-box menu-icon"></i>
-                            </span>
-                            <span class="menu-title">Documentation</span>
-                        </a>
-                    </li>
-                    <li class="nav-item sidebar-user-actions">
-                        <div class="user-details">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div>
-                                    <div class="d-flex align-items-center">
-                                        <div class="sidebar-profile-img">
-                                            <img src="assets/images/faces/face28.png" alt="image">
-                                        </div>
-                                        <div class="sidebar-profile-text">
-                                            <p class="mb-1">Henry Klein</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="badge badge-danger">3</div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item sidebar-user-actions">
-                        <div class="sidebar-user-menu">
-                            <a href="#" class="nav-link"><i class="mdi mdi-settings menu-icon"></i>
-                                <span class="menu-title">Settings</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li class="nav-item sidebar-user-actions">
-                        <div class="sidebar-user-menu">
-                            <a href="#" class="nav-link"><i class="mdi mdi-speedometer menu-icon"></i>
-                                <span class="menu-title">Take Tour</span></a>
-                        </div>
-                    </li>
                     <li class="nav-item sidebar-user-actions">
                         <div class="sidebar-user-menu">
                             <a href="#" class="nav-link"><i class="mdi mdi-logout menu-icon"></i>
                                 <span class="menu-title">Log Out</span></a>
                         </div>
                     </li>
+
+
+
+
                 </ul>
             </nav>
